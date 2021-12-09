0fb7455caf301bb6ec5c65883ebf09e78f29ba7b
Rishi <86615924+Rishikesh1997@users.noreply.github.com>
2021-12-09 16:08:23 +0530

    Add files via upload

---
+++ hooks-posts-user-app/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import UserHooksSample from './UserHooksSample';
+function App() {
+  
+  return (
+    <div className="App">
+      {/* <h1>Sample App Using Hooks</h1>
+       */}
+      {/* <RefParent/> */}
+      I am inside App Component
+      {/* <PortalDemo/> */}
+      {/* <ProfilerDemo/> */}
+      {/* <HooksSample/>  */}
+      <UserHooksSample/>
+    </div>
+  );
+}
+
+export default App;
