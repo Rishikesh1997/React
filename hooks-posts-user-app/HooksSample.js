4b49188b85af0138e5737c901fff0facf34ed72d
Rishi <86615924+Rishikesh1997@users.noreply.github.com>
2021-12-10 11:34:19 +0530

    Add files via upload

--- hooks-posts-user-app/HooksSample.js
+++ hooks-posts-user-app/HooksSample.js
@@ -3,8 +3,15 @@ import { useEffect, useState } from "react";
 
 export default () => {
   const [postsList, setPostsList] = useState([]);
+
   const [title, setTitle] = useState("");
+
   const [body, setBody] = useState("");
+
+  const [postId, setPostId] = useState(0);
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -29,21 +36,68 @@ export default () => {
   const onFormSubmit = (event) => {
     event.preventDefault();
     console.log(title, body);
+    if (!isEditMode) {
+      //code to add new records
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          title,
+          body,
+          userId: 1,
+        })
+        .then((response) => {
+          console.log(response);
+          alert("Added...!");
+          //fetch the data again
+          fetchData("");
+          //reset from
+          setTitle("");
+          setBody("");
+        });
+    } else {
+      //code to edit the records
+      if (postId > 0) {
+        axios
+          .put("https://jsonplaceholder.typicode.com/posts/" + postId, {
+            id: postId,
+            title,
+            body,
+            userId: 1,
+          })
+          .then((response) => {
+            if (response) {
+              fetchData();
+              alert("Updated...!");
+              onResetClickHandler();
+            }
+          });
+      }
+    }
+  };
+
+  const onDeleteClickHandler = (id) => {
     axios
-      .post("https://jsonplaceholder.typicode.com/posts", {
-        title,
-        body,
-        userId: 1,
-      })
+      .delete("https://jsonplaceholder.typicode.com/posts/" + id)
       .then((response) => {
-        console.log(response);
-        alert("Added");
-        //fetch the data again
+        alert("Deleted...!");
         fetchData("");
-        //reset from
-        setTitle("");
-        setBody("");
       });
+  };
+
+  const onEditClickHandler = (postObject) => {
+    console.log(postObject);
+    setIsEditMode(true);
+    setTitle(postObject.title);
+    setBody(postObject.body);
+    setPostId(postObject.id);
+  };
+
+  const onResetClickHandler = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setIsEditMode(false);
+    setTitle("");
+    setBody("");
   };
 
   return (
@@ -56,7 +110,8 @@ export default () => {
         <label>Body:</label>
         <input name="body" value={body} onChange={onBodyChange} />
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit">{isEditMode ? "Update" : "Submit"}</button>
+        {isEditMode && <button onClick={onResetClickHandler}>Reset</button>}
       </form>
       <table>
         <thead>
@@ -73,6 +128,27 @@ export default () => {
                 <td>{post.id}</td>
                 <td>{post.title}</td>
                 <td>{post.body}</td>
+
+                <td>
+                  <button
+                    onClick={() => {
+                      onDeleteClickHandler(post.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+
+                <td>
+                  <button
+                    onClick={() => {
+                      onEditClickHandler(post);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </td>
+                
               </tr>
             );
           })}
--- hooks-posts-user-app/UserHooksSample.js
+++ hooks-posts-user-app/UserHooksSample.js
@@ -2,14 +2,24 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default () => {
+
   const [userList, setUserList] = useState([]);
+
   const [name, setName] = useState("");
+
   const [email, setEmail] = useState("");
+  
+  const [userName, setUserName] = useState("");
+
+  const [id, setId] = useState(0);
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
   useEffect(() => {
-    Fetchdata();
+    fetchData();
   }, []);
 
-  const Fetchdata = () => {
+  const fetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
       console.log(response.data);
       if (response && response.data) {
@@ -17,40 +27,105 @@ export default () => {
       }
     });
   };
+
   const onNameChange = (event) => {
     setName(event.target.value);
   };
+
   const onEmailChange = (event) => {
     setEmail(event.target.value);
   };
+
+  const onUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
-    console.log(name, email);
-    axios
+    console.log(name, email, userName);
+    if(!isEditMode) {
+      axios
       .post("https://jsonplaceholder.typicode.com/users", {
         name,
         email,
-        userId: 1,
+        userName,
+        id: 1,
       })
       .then((response) => {
         console.log(response);
         alert("Added");
-        Fetchdata();
+        fetchData();
         setName("");
         setEmail("");
+        setUserName("");
+      });
+    }
+    else {
+      if(id > 0) {
+        axios.put("https://jsonplaceholder.typicode.com/users/" + id, {
+        name,
+        email,
+        userName,
+        id: 1,
+        })
+        .then((response) => {
+          if (response) {
+            fetchData();
+            alert("Updated...!");
+            onResetClickHandler();
+          }
+      });
+    }
+    }
+  };
+
+  const onDeleteClickHandler = (id) => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + id)
+      .then((response) => {
+        alert("Deleted..!");
+        fetchData("");
       });
   };
+
+  const onEditClickHandler = (userObject) => {
+    console.log(userObject);
+    setIsEditMode(true);
+    setName(userObject.name);
+    setEmail(userObject.email);
+    setUserName(userObject.username);
+    setId(userObject.id);
+  };
+
+  const onResetClickHandler = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setIsEditMode(false);
+    setName("");
+    setEmail("");
+    setUserName("");
+  };
+
 
   return (
     <>
       <h1>From Userhooks</h1>
       <h2>User Form</h2>
       <form onSubmit={onFormSubmit}>
+
         <label>Name</label>
         <input name="name" value={name} onChange={onNameChange} />
+
         <label>Email</label>
         <input name="email" value={email} onChange={onEmailChange} />
-        <button type="submit">Submit</button>
+
+        <label>UserName</label>
+        <input name="userName" value={userName} onChange={onUserNameChange} />
+
+        <button type="submit">{isEditMode ? "Update" : "Submit"}</button>
+        {isEditMode && <button onClick={onResetClickHandler}>Reset</button>}
+
       </form>
       <table>
         <thead>
@@ -58,6 +133,7 @@ export default () => {
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
+            <th>UserName</th>
           </tr>
         </thead>
         <tbody>
@@ -67,6 +143,24 @@ export default () => {
                 <td>{user.id}</td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
+                <td>{user.username}</td>
+
+                <td>
+                  <button onClick={() => {
+                    onDeleteClickHandler(user.id);
+                  }}>Delete</button>
+                </td>
+
+                <td>
+                  <button
+                    onClick={() => {
+                      onEditClickHandler(user);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </td>
+
               </tr>
             );
           })}
