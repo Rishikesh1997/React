51cb0778ade9d0369209729fa9dc4befe9675335
Rishi <86615924+Rishikesh1997@users.noreply.github.com>
2021-12-18 10:58:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Body from './Body';
-import Footer from './Footer';
-import Header from './Header';
-import Nav from './Nav';
-import Posts from './Posts';
-import ToDos from './ToDos';
-import Users from './Users';
+import React from "react";
+// import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <>
-    
-    <Header />
-    <Nav />
-    <Routes>
-      <Route path={'/todos'} element={<ToDos />} />
-      <Route path={'/posts'} element={<Posts />} />
-      <Route path={'/users'} element={<Users />} />
-    </Routes>
-    {/* <Body />
-    <Footer /> */}
-    </>
-  );
-}
+const App = () => (
+  <>
+    <h1>Hello React</h1>
+    <p>Minimal React configuration.</p>
+  </>
+);
 
 export default App;
+// ReactDOM.render(<App />, document.getElementById("root"));
